49fddda8475eb3df035a68a7d63f0ad8ea1d60f9
sanghoon <sanghoon@DESKTOP-R4D60AP>
2021-07-20 23:18:29 +0900

    hwjava01_부울경4반_차상훈

--- 0719/CircleArea.java
+++ 0719/CircleArea.java
@@ -3,7 +3,7 @@ package com.java.first;
 public class CircleArea {
 
 	public static void main(String[] args) {
-		float r=5f, pi=3.14f, area;
+		float r=5.0f, pi=3.14f, area;
 		
 		area = (r * r) * pi;
 		System.out.println("반지름이 5Cm인 원의 넓이는 "+ area + "Cm2입니다.");
